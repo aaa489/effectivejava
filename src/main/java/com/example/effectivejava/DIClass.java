6585f6e43057cf8390d4be76c7d82180cb1abe82
don <wangh@fingard.com>
2022-03-02 19:38:01 +0800

    添加已完成的部分目录

--- src/main/java/com/example/effectivejava/DIClass.java
+++ src/main/java/com/example/effectivejava/DIClass.java
@@ -3,7 +3,7 @@ package com.example.effectivejava;
 import java.util.Objects;
 
 /**
- * 《effective java》-6：依赖注入优于硬连接资源（hardwiring resources）
+ * 《effective java》-5：依赖注入优于硬连接资源（hardwiring resources）
  * 如果实现依赖于底层资源，不要使用工具类或单例类引入，而使用依赖注入模式，具体资源由外部来注入，这样更灵活
  * @author Don
  * @date 2022/3/1.
